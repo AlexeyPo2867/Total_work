df64faaa220ce282acb062c78a8317804582ef51
alexey <shaman1801@yandex.ru>
2022-11-20 17:07:07 +0300

    add program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите через пробел несколько строк");
+string Str = Console.ReadLine();
+string[] arrStr = Str.Split(' ');
+
+for (int i = 0; i < arrStr.Length; i++)
+{
+    if (arrStr[i].Length <= 3)
+        Console.WriteLine(arrStr[i]);
+}
